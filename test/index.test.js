b5519e1eec30aa10aabe1fe6f674ed917d6c7b48
hnamazli <hasan@namazli.com>
2019-12-04 11:25:11 +0400

    added test config

---
+++ test/index.test.js
@@ -0,0 +1,25 @@
+const { assert } = require('chai');
+//const  = require('../src/');
+
+describe('', () => {
+    it('Should be an object', () => {
+        //const  = new ();
+        
+        assert.isObject();
+    });
+});
+
+describe('BSTree.getSize', () => {
+    beforeEach(() => {
+        //
+    });
+
+    it('should return size (undefined)', () => {
+        const arr = undefined;
+        const expected = 0;
+
+        const actual = 0;
+
+        assert.deepEqual(actual, expected);
+    });
+});
